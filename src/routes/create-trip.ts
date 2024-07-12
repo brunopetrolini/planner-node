08caa0975dc405b34fe4b1b5fd47c9faa2739b69
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-07-12 15:01:36 -0300

    feat: adds date validation

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,6 +1,7 @@
 import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
-
 import { z } from 'zod'
+import dayjs from 'dayjs'
+
 import { prisma } from '../lib/prisma'
 
 const createTripSchema = z.object({
@@ -12,6 +13,18 @@ const createTripSchema = z.object({
 export async function createTrip(app: FastifyInstance) {
   app.post('/trips', async (request: FastifyRequest, reply: FastifyReply) => {
     const payload = createTripSchema.parse(request.body)
+
+    const isStartsDateBeforeNow = dayjs(payload.startsAt).isBefore(dayjs())
+    if (isStartsDateBeforeNow) {
+      return reply.status(400).send({ error: 'Invalid start date.' })
+    }
+
+    const isEndsDateBeforeStartsDate = dayjs(payload.endsAt).isBefore(
+      payload.startsAt,
+    )
+    if (isEndsDateBeforeStartsDate) {
+      return reply.status(400).send({ error: 'Invalid end date.' })
+    }
 
     const trip = await prisma.trip.create({ data: payload })
 
