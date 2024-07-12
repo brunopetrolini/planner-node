92147376b24062011b9da384c258fe2b84e4ca07
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-07-12 16:05:43 -0300

    feat: adds cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 
 import { createTrip } from './routes/create-trip'
 import { confirmTrip } from './routes/confirm-trip'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: '*',
+})
 
 app.register(createTrip)
 app.register(confirmTrip)
