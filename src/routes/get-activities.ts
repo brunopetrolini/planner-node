6a79a568a8772dc131607ae15da42bf528a5686f
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-07-14 12:15:31 -0300

    feat: adds activivies filter by date

--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -2,6 +2,7 @@ import type { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 import { prisma } from '../lib/prisma'
+import { dayjs } from '../lib/dayjs'
 
 const createActivityParamsSchema = z.object({
   tripId: z.string().uuid(),
@@ -22,7 +23,24 @@ export async function getActivities(app: FastifyInstance) {
         return reply.status(404).send({ error: 'Trip not found.' })
       }
 
-      return reply.status(200).send({ activities: trip.activities })
+      const differenceInDaysBetweenStartAndEnd = dayjs(trip.endsAt).diff(
+        trip.startsAt,
+        'days',
+      )
+
+      const activities = Array.from({
+        length: differenceInDaysBetweenStartAndEnd + 1,
+      }).map((_, index) => {
+        const date = dayjs(trip.startsAt).add(index, 'days')
+        return {
+          date: date.toDate(),
+          activities: trip.activities.filter((activity) => {
+            return dayjs(activity.occursAt).isSame(date, 'day')
+          }),
+        }
+      })
+
+      return reply.status(200).send({ activities })
     },
   )
 }
